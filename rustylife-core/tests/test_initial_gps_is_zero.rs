41dd9b3af57fef76598dd5c4335a9d4927b00d97
ID-Rocketeer <11449801+ID-Rocketeer@users.noreply.github.com>
2026-02-26 22:44:49 -0600

    test: remove unused Engine import in initial gps test

--- rustylife-core/tests/test_initial_gps_is_zero.rs
+++ rustylife-core/tests/test_initial_gps_is_zero.rs
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use rustylife_core::engine::{Engine, EngineSubscriber, SimulationEngine};
+use rustylife_core::engine::{EngineSubscriber, SimulationEngine};
 use rustylife_core::space::SimulationSpace;
 use std::sync::{Arc, Mutex};
 
